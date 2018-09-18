8b01876f533f2188b43e7c31152a4b53ef4407fa
Erik-Lân Do Dinh <dodinh@ukp.informatik.tu-darmstadt.de>
2018-09-18 10:52:13 +0200

    Fix missing import.

--- src/main/java/org/adho/dhconvalidator/ui/PaperSelectionPanel.java
+++ src/main/java/org/adho/dhconvalidator/ui/PaperSelectionPanel.java
@@ -21,6 +21,7 @@ import com.vaadin.ui.Table;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
